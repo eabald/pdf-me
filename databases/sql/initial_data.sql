6f80a56ab2cf27cd3999fdadac9799fb388b73b7
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-06-10 22:12:52 +0200

    fixed typo in sql initial data

--- databases/sql/initial_data.sql
+++ databases/sql/initial_data.sql
@@ -21,7 +21,7 @@ USE `pdf_me`;
 -- Table structure for table `private_keys`
 --
 
-DROP TABLE IF EXISTS `private_keys`;a
+DROP TABLE IF EXISTS `private_keys`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `private_keys` (
