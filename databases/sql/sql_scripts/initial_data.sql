94c57d253a81b49de148f761632f64464eaf74cc
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-06-13 20:49:31 +0200

    db test data update

--- databases/sql/sql_scripts/initial_data.sql
+++ databases/sql/sql_scripts/initial_data.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `pdf_me` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `pdf_me`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pdf_me
@@ -18,6 +16,34 @@ USE `pdf_me`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `pdfs`
+--
+
+DROP TABLE IF EXISTS `pdfs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pdfs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `filename` varchar(512) NOT NULL,
+  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_fk_idx` (`user_id`),
+  CONSTRAINT `` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pdfs`
+--
+
+LOCK TABLES `pdfs` WRITE;
+/*!40000 ALTER TABLE `pdfs` DISABLE KEYS */;
+INSERT INTO `pdfs` VALUES (1,'dc49fdc7-cc6a-11eb-8ade-0242ac1200021623609321729.pdf',NULL,1),(2,'dc49fdc7-cc6a-11eb-8ade-0242ac1200021623609768263.pdf',NULL,1),(3,'dc49fdc7-cc6a-11eb-8ade-0242ac1200021623609904199.pdf','2021-06-13 18:45:04',1);
+/*!40000 ALTER TABLE `pdfs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `private_keys`
 --
 
@@ -27,8 +53,11 @@ DROP TABLE IF EXISTS `private_keys`;
 CREATE TABLE `private_keys` (
   `id` int NOT NULL AUTO_INCREMENT,
   `key` varchar(2048) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +66,7 @@ CREATE TABLE `private_keys` (
 
 LOCK TABLES `private_keys` WRITE;
 /*!40000 ALTER TABLE `private_keys` DISABLE KEYS */;
+INSERT INTO `private_keys` VALUES (1,'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',1);
 /*!40000 ALTER TABLE `private_keys` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,10 +81,11 @@ CREATE TABLE `templates` (
   `id` int NOT NULL AUTO_INCREMENT,
   `template` text NOT NULL,
   `user_id` int NOT NULL,
+  `uuid` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,8 +94,24 @@ CREATE TABLE `templates` (
 
 LOCK TABLES `templates` WRITE;
 /*!40000 ALTER TABLE `templates` DISABLE KEYS */;
+INSERT INTO `templates` VALUES (1,'<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Document</title></head><body>{{test}}</body></html>',1,'1a24257a-cc6b-11eb-8ade-0242ac120002');
 /*!40000 ALTER TABLE `templates` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `before_insert_templates` BEFORE INSERT ON `templates` FOR EACH ROW SET new.uuid = uuid() */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `users`
@@ -76,11 +123,8 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `uuid` varchar(256) DEFAULT NULL,
-  `key_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `key_id_idx` (`key_id`),
-  CONSTRAINT `key_id` FOREIGN KEY (`key_id`) REFERENCES `private_keys` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,6 +133,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'dc49fdc7-cc6a-11eb-8ade-0242ac120002');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -124,4 +169,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-09 16:12:01
+-- Dump completed on 2021-06-13 20:47:23
